2a6efc545302630303581de398c180693d2d63e2
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2025-10-03 19:02:00 +0530

    Updates

---
+++ _next/static/chunks/6f001cfcf214ebc1.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/4814141185d1722a.js",
+  "static/chunks/turbopack-29c22f762da77118.js"
+])
---
+++ _next/static/chunks/e710c875d0905a55.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/4814141185d1722a.js",
+  "static/chunks/turbopack-18b032d017608a01.js"
+])
